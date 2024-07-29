f5253dc651198e5c26eda21af2be523302a23e77
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-07-29 18:35:35 +0100

    Remove su.azure and params, tweak design

---
+++ R/azure.R
@@ -0,0 +1,65 @@
+# Functions here copied from the private {su.azure} package to avoid GitHub PAT
+# headaches when trying to deploy the Rmd to Connect.
+
+get_nhp_result_sets <- function(
+    container,
+    allowed_datasets = get_nhp_user_allowed_datasets(NULL),
+    folder = "prod"
+) {
+
+  allowed <- tibble::tibble(dataset = allowed_datasets)
+
+  container |>
+    AzureStor::list_blobs(folder, info = "all", recursive = TRUE) |>
+    dplyr::filter(!.data[["isdir"]]) |>
+    purrr::pluck("name") |>
+    purrr::set_names() |>
+    purrr::map(\(name, ...) AzureStor::get_storage_metadata(container, name)) |>
+    dplyr::bind_rows(.id = "file") |>
+    dplyr::semi_join(allowed, by = dplyr::join_by("dataset")) |>
+    dplyr::mutate(dplyr::across("viewable", as.logical))
+
+}
+
+get_container <- function(
+    tenant = Sys.getenv("AZ_TENANT_ID"),
+    app_id = Sys.getenv("AZ_APP_ID"),
+    ep_uri = Sys.getenv("AZ_STORAGE_EP"),
+    container_name = Sys.getenv("AZ_STORAGE_CONTAINER")
+) {
+
+  # if the app_id variable is empty, we assume that this is running on an Azure VM,
+  # and then we will use Managed Identities for authentication.
+  token <- if (app_id != "") {
+    AzureAuth::get_azure_token(
+      resource = "https://storage.azure.com",
+      tenant = Sys.getenv("AZ_TENANT_ID"),
+      app = app_id,
+      auth_type = "device_code"
+    )
+  } else {
+    AzureAuth::get_managed_token("https://storage.azure.com/") |>
+      AzureAuth::extract_jwt()
+  }
+
+  ep_uri |>
+    AzureStor::blob_endpoint(token = token) |>
+    AzureStor::storage_container(container_name)
+
+}
+
+get_nhp_user_allowed_datasets <- function(groups = NULL) {
+
+  p <- system.file("extdata", "providers.json", package = "su.azure") |>
+    jsonlite::read_json(simplifyVector = TRUE)
+
+  if (!(is.null(groups) || any(c("nhp_devs", "nhp_power_users") %in% groups))) {
+    a <- groups |>
+      stringr::str_subset("^nhp_provider_") |>
+      stringr::str_remove("^nhp_provider_")
+    p <- intersect(p, a)
+  }
+
+  c("synthetic", p)
+
+}
--- R/table.R
+++ R/table.R
@@ -7,6 +7,15 @@ create_reactable <- function(dat) {
     extensions = "Buttons",
     options = list(
       dom = "Bfrtip",
+      autoWidth = TRUE,
+      columnDefs = list(
+        list(width = "100px", targets = 0),
+        list(width = "100px", targets = 1),
+        list(width = "100px", targets = 2),
+        list(width = "75px",  targets = 3),
+        list(width = "75px",  targets = 4),
+        list(width = "75px",  targets = 5)
+      ),
       pageLength = 5,
       columnDefs = list(list(targets = 5, searchable = FALSE)),
       buttons = list(
