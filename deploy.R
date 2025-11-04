9d934aca096d67684a91fdb389398638b75d18ea
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2025-11-04 18:01:55 +0000

    Deploy to current server only

--- deploy.R
+++ deploy.R
@@ -1,18 +1,9 @@
-# Deploy to the current and new Posit Connect servers. The current server
-# (strategyunitwm.nhs.uk) will be switched off in May/June 2025 and the new
-# server (currently named su.mlcsu.org) will take its name.
-
-deploy <- function(server_name, app_id) {
-  rsconnect::deployDoc(
-    server = server_name,
-    appId = app_id,
-    doc = "index.Rmd",
-    appName = "nhp_tagged_runs",
-    appTitle = "NHP: tagged-runs and sites tables",
-    lint = FALSE,
-    forceUpdate = TRUE
-  )
-}
-
-deploy("connect.strategyunitwm.nhs.uk", 287)
-deploy("connect.su.mlcsu.org", 112)
+rsconnect::deployDoc(
+  server = "connect.strategyunitwm.nhs.uk",
+  appId = 112,
+  doc = "index.Rmd",
+  appName = "nhp_tagged_runs",
+  appTitle = "NHP: tagged-runs and sites tables",
+  lint = FALSE,
+  forceUpdate = TRUE
+)
