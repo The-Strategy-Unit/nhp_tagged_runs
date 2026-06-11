8657b5445e4a31dc630b1b49bc14ededcf4ed73b
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2026-06-11 18:11:42 +0100

    Update env-var-related references

--- deploy.R
+++ deploy.R
@@ -4,6 +4,13 @@ rsconnect::deployDoc(
   doc = "index.Rmd",
   appName = "nhp_tagged_runs",
   appTitle = "NHP: scenario run-stage table",
+  envVars = c(
+    "AZ_CONTAINER_SUPPORT",
+    "AZ_STORAGE_EP",
+    "AZ_TABLE_EP",
+    "AZ_TABLE_NAME",
+    "NHP_ENCRYPT_KEY"
+  ),
   lint = FALSE,
   forceUpdate = TRUE
 )
